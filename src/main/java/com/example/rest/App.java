4af99c6594f8b940d5c036dd222dfa4e9a79bd11
WALID NEJI <walid.neji@adservio.fr>
2018-07-03 12:31:24 +0200

    attack :fire:

--- src/main/java/com/example/rest/App.java
+++ src/main/java/com/example/rest/App.java
@@ -42,7 +42,7 @@ public class App {
 
         System.out.println("Server started");
 
-        //runGetRequest();
+        runGetRequest();
     }
 
     /**
@@ -51,7 +51,7 @@ public class App {
     private static void runGetRequest() {
 
     	// sample URL
-        String url = "http://ip.jsontest.com/";
+        String url = "http://129.157.179.180:3000/shield/33/45/red/adserviops";
         CloseableHttpResponse response = null;
 
         try {
