5ebbf8228625cdd485d1e352daac50a9a9ccc711
WALID NEJI <walid.neji@adservio.fr>
2018-07-03 12:11:29 +0200

    my first microservice

--- src/main/java/com/example/rest/App.java
+++ src/main/java/com/example/rest/App.java
@@ -1,31 +1,33 @@
 /* Copyright © 2016 Oracle and/or its affiliates. All rights reserved. */
 package com.example.rest;
- 
+
 import java.util.Optional;
 import java.util.Properties;
 import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
- 
+
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.util.EntityUtils;
- 
+
+// My microservice!
+
 @SpringBootApplication
 public class App {
- 
+
     // Get PORT and HOST from Environment or set default
     public static final Optional<String> host;
     public static final Optional<String> port;
     public static final Properties myProps = new Properties();
- 
+
     static {
         host = Optional.ofNullable(System.getenv("HOSTNAME"));
         port = Optional.ofNullable(System.getenv("PORT"));
     }
- 
+
     /**
      * Start the server.
      */
@@ -33,25 +35,25 @@ public class App {
         // Set properties
         myProps.setProperty("server.address", host.orElse("localhost"));
         myProps.setProperty("server.port", port.orElse("8080"));
- 
+
         SpringApplication app = new SpringApplication(App.class);
         app.setDefaultProperties(myProps);
         app.run(args);
- 
+
         System.out.println("Server started");
- 
+
         //runGetRequest();
     }
- 
+
     /**
      * Performs a simple GET request and prints the result to the log.
      */
     private static void runGetRequest() {
- 
+
     	// sample URL
         String url = "http://ip.jsontest.com/";
         CloseableHttpResponse response = null;
- 
+
         try {
             CloseableHttpClient httpclient = HttpClients.createDefault();
             HttpGet httpGet = new HttpGet(url);
@@ -68,6 +70,6 @@ public class App {
             } catch (IOException ie) {
                 System.out.println(ie);
             }
-        }    
+        }
     }
 }
